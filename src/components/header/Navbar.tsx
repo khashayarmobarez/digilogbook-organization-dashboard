a0d87763fa601aa9133ffa500907a0ff5a4810a1
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-08-14 14:47:31 +0330

    chore: Remove commented out code and update dependencies

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-
-import digilogbookLogo from '../../../public/svgs/Digilogbook -1401 1.svg'
 import Image from 'next/image';
+
+// assets and mui
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import LoginIcon from '@mui/icons-material/Login';
+import digilogbookLogo from '../../../public/svgs/Digilogbook -1401 1.svg'
+
 
 export default function Navbar() {
     return (
-        <div className="navbar  bg-navbar-gradient-shadow absolute top-0 z-50 h-14 md:h-20 ">
+        <div className="navbar  bg-navbar-gradient-shadow flex justify-between absolute top-0 z-50 h-14 md:h-20 lg:px-10 ">
             <div className="flex-none w-2/6 md:w-1/12">
                 <button className="btn btn-square btn-ghost w-full">
                     <Image src={digilogbookLogo} alt="digilogbookLogo" width={100} height={100} />
                 </button>
             </div>
-            <div className="flex-1">
-                
+            <div className=" flex gap-x-2 px-2">
+                <LoginIcon sx={{fill:'var(#ebebf0)'}} />
+                <NotificationsOutlinedIcon sx={{fill:'var(#ebebf0)', height:'30px',width:'30px'}} />
             </div>
         </div>
     );

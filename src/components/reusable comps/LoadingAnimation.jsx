bfce78f822d6fd4a8bb9c6ecb7fbbe9653d9dac0
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-08-19 17:28:19 +0330

    added loading animation

---
+++ src/components/reusable comps/LoadingAnimation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingAnimation = () => {
+    return (
+        <>
+            <span className="loading loading-infinity loading-lg"></span>
+        </>
+    );
+};
+
+export default LoadingAnimation;

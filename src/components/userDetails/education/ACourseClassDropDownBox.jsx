eef916d16beb5c96f842adb060b01dc400bade29
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-08-21 11:37:40 +0330

    fixed the bug

--- src/components/userDetails/education/ACourseClassDropDownBox.jsx
+++ src/components/userDetails/education/ACourseClassDropDownBox.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 // styles
 import container from '@/styles/Containers.module.css';
 
+// assets
+import bookIcon from '@/../public/svgs/book.svg';
+
 // queries
 import { useACourseClass } from '@/api/courseDetails';
 import Image from 'next/image';
@@ -12,18 +15,16 @@ const ACourseClassDropDownBox = ({classData,userId}) => {
     const {  data: classDetails, isLoading: classDetailsLoading, error: classDetailsError } = useACourseClass(classData.id,userId);
 
     return (
-        <div className={`${container.container2withHover} w-full min-h-12 flex justify-between items-center pl-10 pr-4 md:pl-40 rounded-2xl`}>
+        <div className={`${container.container2withHover} w-full min-h-12 flex justify-start gap-x-[20%] items-center pl-10 pr-4 md:pl-40 rounded-2xl`}>
             {
                 classData &&
                 <>
 
-                    <Image  src={calender} alt='icon' />
+                    <Image  src={bookIcon} alt='icon' />
 
                     <p>{classData.name} </p>
                     
-                    <p>{classData.classDuration}</p>
-
-                    <div/>
+                    <p className='text-xs'>{classData.classDuration}</p>
 
                 </>
             }

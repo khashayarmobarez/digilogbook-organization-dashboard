1183db0204d88e7a5fbcb45dd7e1c632f8e116b1
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-08-29 14:49:51 +0330

    finished coach details

--- src/app/dashboard/clubs/[clubId]/coaches/coachDetails/[coachId]/page.jsx
+++ src/app/dashboard/clubs/[clubId]/coaches/coachDetails/[coachId]/page.jsx
@@ -21,7 +21,7 @@ const CoachDetails = ({params}) => {
     }
 
     return (
-        <div className=' w-full flex flex-col gap-y-7 items-center py-6 md:grid md:grid-cols-4'>
+        <div className=' w-full flex flex-col gap-y-7 items-center pb-6 md:grid md:grid-cols-2'>
             
             {  clubCoachesCoursesError &&
                 <p className='w-full text-center'>مشکلی پیش اماده, دوباره تلاش کنید</p>
@@ -31,7 +31,7 @@ const CoachDetails = ({params}) => {
                  clubCoachesCoursesData && clubCoachesCoursesData.data?.map((courseData, index) => (
                     <div key={index} className='w-full flex flex-col items-center'>
 
-                        <div className={`${containers.darkMainContainer} rounded-3xl h-auto z-0 w-[98%] md:w-full flex flex-col justify-between items-center px-4 py-4 gap-y-4 mr-1 mt-1`}>
+                        <div className={`${containers.darkMainContainer} rounded-3xl h-auto z-0 md:w-full flex flex-col justify-between items-center px-4 py-4 gap-y-4 mr-1 mt-1`}>
 
 
                             <div className='w-full flex justify-between'>
@@ -51,9 +51,43 @@ const CoachDetails = ({params}) => {
                                 >{courseData.status}</p>
                             </div>
 
-                            <div className='w-full flex justify-between text-start text-sm'>
-                                <div className='flex flex-col justify-between self-start'>
-                                    { courseData.organization && courseData.type !== 'Regular' &&
+                            <div className='w-full flex flex-col justify-between text-start text-sm gap-y-4'>
+
+                                <div className=' w-full justify-between self-start grid grid-cols-2 gap-2 text-center'>
+                                    
+                                    { courseData.level &&
+                                        <p>
+                                            <span className='text-lowOpacityWhite'>
+                                            مقطع:&nbsp;
+                                            </span>
+                                                {courseData.level}
+                                        </p>
+                                    }
+                                    { courseData.activeStudentCounts &&
+                                        <p>
+                                            <span className='text-lowOpacityWhite'>
+                                            تعداد هنرجویان فعال:&nbsp;
+                                            </span>
+                                                {courseData.activeStudentCounts}
+                                        </p>
+                                    }
+                                    { courseData.flightsCount >= 0 &&
+                                        <p>
+                                            <span className='text-lowOpacityWhite'>
+                                            تعداد پرواز:&nbsp;
+                                            </span>
+                                            {courseData.flightsCount}
+                                        </p>
+                                    }
+                                    { courseData.historyStudentCounts >= 0 &&
+                                        <p>
+                                            <span className='text-lowOpacityWhite'>
+                                            تعداد هنرجویان سابق:&nbsp;
+                                            </span>
+                                            {courseData.historyStudentCounts}
+                                        </p>
+                                    }
+                                    { courseData.organization &&
                                         <p>
                                             <span className='text-lowOpacityWhite'>
                                                 ارگان:&nbsp;
@@ -61,19 +95,19 @@ const CoachDetails = ({params}) => {
                                                 {courseData.organization}
                                         </p>
                                     }
-                                    { courseData.coach &&
+                                    { courseData.coachFullName &&
                                         <p>
                                             <span className='text-lowOpacityWhite'>
                                                 مربی:&nbsp;
                                             </span> 
-                                            {courseData.coach}
+                                            {courseData.coachFullName}
                                         </p>
                                     }
                                 </div>
                                 
                                 <button 
                                 onClick={handleCourseDetails(courseData.id)}
-                                className={`${ButtonStyles.normalButton} min-w-28 self-end`} >
+                                className={`${ButtonStyles.normalButton} min-w-28 self-center`} >
                                     جزئیات  
                                 </button>
 

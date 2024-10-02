4c6856272878e5d8298a79fb43bb8ccf931eda8b
khashayar mobarez <khashayarmobarez333@gmail.com>
2024-10-02 17:07:34 +0330

    added 404 not found animation

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
     return (
         <div className='w-full flex flex-col h-[100dvh] justify-center items-center text-2xl pt-10'>
             <p>صفحه مورد نظر یافت نشد || 404</p>
-            {/* <Image src={notfoundAnimation} alt='not found animation' /> */}
+            <Image src={notfoundAnimation} alt='not found animation' />
         </div>
     );
 };
